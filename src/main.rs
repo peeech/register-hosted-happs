695af84edc5e7010c66f81d83c562ed50e5a8df9
PJ <pj@imagine-nyc.com>
2020-03-29 22:14:27 +0200

    read toml file from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use serde_derive::{Serialize, Deserialize};
+use std::io::{stdin, Read};
+
+#[derive(Serialize, Deserialize)]
+struct Happs {
+    hhaids: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    stdin().read_to_string(&mut buffer).unwrap();
+
+    let happs :Happs = toml::from_str(&buffer).unwrap();
+
+    let happs_json = serde_json::to_string(&happs).unwrap();
+
+
+
 }
